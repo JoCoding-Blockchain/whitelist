22fb27f5f2ca24c0b1a663d4f280b40827e80f08
youtube-jocoding <adjocoding@gmail.com>
2022-05-28 23:34:28 +0900

    rootHash 입력

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ import whitelist from './whitelist.js';
 try {
     const leafNodes = whitelist.map(addr => keccak256(addr));
     const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-    const rootHash = '0x' + merkleTree.getRoot().toString('hex');
+
+    const rootHash = ""
 
     const addr = keccak256("");
     const hexProof = merkleTree.getHexProof(addr);
